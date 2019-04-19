846e4a9c3ba1a07b3792e089bfe1e51356742089
Marcel Vroone <mvroone@it-actief.nl>
2019-04-19 21:49:25 +0200

    Added IEnumerable<string>.join

---
+++ src/mondop.core.tests/IEnumerableStringExentionsTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Mondop.Core.Tests
+{
+    [TestClass]
+    public class IEnumerableStringExentionsTests
+    {
+        [TestMethod]
+        public void CallJoin_WithNull_Expect_Null()
+        {
+            string[] temp = null;
+            var result = temp.Join(",");
+
+            result.Should().BeNull();
+        }
+
+        [TestMethod]
+        public void CallJoin_WithNullSeparation_Expect_ArgumentNullException()
+        {
+            Action action = () => { new string[] { "A", "B", "C" }.Join(null); };
+
+            action.Should().ThrowExactly<ArgumentNullException>();
+        }
+
+        [TestMethod]
+        public void CallJoin_WithCorrectParametes_Expect_JoinedString()
+        {
+            var array = new string[] { "A", "B", "C" };
+
+            var result = array.Join("x");
+            result.Should().Be("AxBxC");
+        }
+    }
+}
---
+++ src/mondop.core/IEnumerableExentions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Mondop.Core
+{
+    public static class IEnumerableStringExentions
+    {
+        public static string Join(this IEnumerable<string> values,string separator)
+        {
+            Ensure.IsNotNull(separator, nameof(separator));
+
+            if (values == null)
+                return null;
+
+            return string.Join(separator, values);
+        }
+    }
+}

f4ab332b0c8800a34721dc2bd0b78c232abe40d8
Marcel Vroone <mvroone@it-actief.nl>
2020-02-01 16:47:32 +0100

    Added StringBuilderExtensions

---
+++ src/mondop.core/StringBuilderExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+
+namespace Mondop.Core
+{
+    public static class StringBuilderExtensions
+    {
+        public static void AppendLineIfNotNullOrWhiteSpace(this StringBuilder stringBuilder, string textToAppend)
+        {
+            if (!string.IsNullOrWhiteSpace(textToAppend))
+                stringBuilder.AppendLine(textToAppend);
+        }
+    }
+}
